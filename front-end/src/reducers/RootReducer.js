c9d41cf2aeca78411ec98167266920e1a0b7aec3
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-12-20 09:50:01 -0500

    Auth added to root

--- front-end/src/reducers/RootReducer.js
+++ front-end/src/reducers/RootReducer.js
@@ -2,14 +2,14 @@ import { combineReducers } from 'redux';
 
 // import each individual reducer to hand to combineReducers
 // first: AuthReducer
-// import AuthReducer from './AuthReducer';
+import AuthReducer from './AuthReducer';
 
 
 
 //combreducers takes an object as an argument, that arg has key:value pair ... will return a value
 const rootReducer = combineReducers({	
-	
-})
+	auth: AuthReducer
+});
 
 
 export default rootReducer;
