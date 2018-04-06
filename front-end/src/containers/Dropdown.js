f354b42fe41bb671e4b783ad11519c56f943f2de
Valerie Jane <valeriejanethoma@gmail.com>
2018-04-06 10:23:04 -0400

    rewrite login spec, add eslintrc

--- front-end/src/containers/Dropdown.js
+++ front-end/src/containers/Dropdown.js
@@ -13,9 +13,9 @@ export default function () {
 				<Button className="dropButton">Select<Icon id='circleDown' icon={circleDown} /></Button>
 			}>
 				<Link to='/login/parent' data-cy="parent"><NavItem>Parent</NavItem></Link>
-				<Link to='/login/teacher'><NavItem>Teacher</NavItem></Link>
+				<Link to='/login/teacher' data-cy="teacher"><NavItem>Teacher</NavItem></Link>
 				<NavItem divider />
-				<Link to='/login/student'><NavItem>Student</NavItem></Link>
+				<Link to='/login/student'data-cy="student"><NavItem>Student</NavItem></Link>
 			</Dropdown>
 		</div>
 
